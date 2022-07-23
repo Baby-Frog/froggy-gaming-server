d193701126d27ed16c770f20bdda2a9ad3a64539
QuangMinh <93485271+QuangMinh8699@users.noreply.github.com>
2022-07-23 15:23:48 +0700

    first commit

---
+++ src/main/java/com/forggygaming/froggygamingserver/FroggyGamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.forggygaming.froggygamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FroggyGamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FroggyGamingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forggygaming/froggygamingserver/FroggyGamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forggygaming.froggygamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FroggyGamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
