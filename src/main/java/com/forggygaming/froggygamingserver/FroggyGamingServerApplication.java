fc4f291da7ff406c652aca6278230775462aa35d
Võ Văn Việt <vietvvth2107012@fpt.edu.vn>
2022-09-06 15:07:26 +0700

    Update FroggyGamingServerApplication.java

--- src/main/java/com/forggygaming/froggygamingserver/FroggyGamingServerApplication.java
+++ src/main/java/com/forggygaming/froggygamingserver/FroggyGamingServerApplication.java
@@ -1,8 +1,13 @@
 package com.forggygaming.froggygamingserver;
 
 //import com.forggygaming.froggygamingserver.service.UserServices;
+import com.forggygaming.froggygamingserver.entity.Customer;
+import com.forggygaming.froggygamingserver.entity.Role;
+import com.forggygaming.froggygamingserver.service.CustomerServices;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 //import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -15,22 +20,22 @@ public class FroggyGamingServerApplication {
     }
 
 //    @Bean
-//    CommandLineRunner run(UserServices userServices){
+//    CommandLineRunner run(CustomerServices customerServices){
 //        return args -> {
-//            userServices.saveRole(new Role(1,"ROLE_USER"));
-//            userServices.saveRole(new Role(2,"ROLE_ADMIN"));
-//            userServices.saveRole(new Role(3,"ROLE_MANAGER"));
-//            userServices.saveRole(new Role(4,"ROLE_SUPER_ADMIN"));
+//            customerServices.saveRole(new Role(1,"ROLE_USER"));
+//            customerServices.saveRole(new Role(2,"ROLE_ADMIN"));
+//            customerServices.saveRole(new Role(3,"ROLE_MANAGER"));
+//            customerServices.saveRole(new Role(4,"ROLE_SUPER_ADMIN"));
 //
-//            userServices.saveUser(new Users("viet","vietvo","123",new ArrayList<>(),new Customer()));
-//            userServices.saveUser(new Users("nam","namdan","123",new ArrayList<>(),new Customer()));
-//            userServices.saveUser(new Users("minh","minhnhi","123",new ArrayList<>(),new Customer()));
-//            userServices.saveUser(new Users("khoi","khoingan","123",new ArrayList<>(),new Customer()));
+//            customerServices.insertCustomer(new Customer("vietvo","123"));
+//            customerServices.insertCustomer(new Customer("namdan","123"));
+//            customerServices.insertCustomer(new Customer("minhnhi","123"));
+//            customerServices.insertCustomer(new Customer("khoingan","123"));
 //
-//            userServices.addRoleToUser("vietvo","ROLE_USER");
-//            userServices.addRoleToUser("namdan","ROLE_USER");
-//            userServices.addRoleToUser("minhnhi","ROLE_ADMIN");
-//            userServices.addRoleToUser("khoingan","ROLE_SUPER_ADMIN");
+//            customerServices.addRoleToCustomer("vietvo","ROLE_USER");
+//            customerServices.addRoleToCustomer("namdan","ROLE_USER");
+//            customerServices.addRoleToCustomer("minhnhi","ROLE_ADMIN");
+//            customerServices.addRoleToCustomer("khoingan","ROLE_SUPER_ADMIN");
 //        };
 //    }
 }
