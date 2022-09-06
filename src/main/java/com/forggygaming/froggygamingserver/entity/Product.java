d0f66417d1231dab58fbf420648ff5f95181eb5e
QuangMinh <93485271+QuangMinh8699@users.noreply.github.com>
2022-09-06 14:45:56 +0700

    Update Product.java

--- src/main/java/com/forggygaming/froggygamingserver/entity/Product.java
+++ src/main/java/com/forggygaming/froggygamingserver/entity/Product.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Nationalized;
 
 import javax.persistence.*;
 import javax.transaction.Transactional;
