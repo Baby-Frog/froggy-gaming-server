b24a61d8d391c693ad8dd19ba7f21004874d6cb6
QuangMinh <93485271+QuangMinh8699@users.noreply.github.com>
2022-09-15 00:42:52 +0700

    Update LoginController.java

--- src/main/java/com/forggygaming/froggygamingserver/controller/LoginController.java
+++ src/main/java/com/forggygaming/froggygamingserver/controller/LoginController.java
@@ -1,4 +1,5 @@
 package com.forggygaming.froggygamingserver.controller;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,52 +29,52 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 public class LoginController {
-        @Autowired
-        AuthenticationManager authenticationManager;
-        @Autowired
-        CustomerServices customerServices;
-        @Autowired
-        CustomerRepo customerRepo;
-        @Autowired
-        RoleRepo roleRepository;
-        @Autowired
-        PasswordEncoder encoder;
-        @Autowired
-         JwtUtils jwtUtils;
-        @PostMapping("/signin")
-        public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
-            UsernamePasswordAuthenticationToken obj =
-                    new UsernamePasswordAuthenticationToken(
-                            loginRequest.getUsername(),
-                            loginRequest.getPassword());
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    CustomerServices customerServices;
+    @Autowired
+    CustomerRepo customerRepo;
+    @Autowired
+    RoleRepo roleRepository;
+    @Autowired
+    PasswordEncoder encoder;
+    @Autowired
+    JwtUtils jwtUtils;
 
-            Authentication authentication = authenticationManager.authenticate(obj);
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        UsernamePasswordAuthenticationToken obj =
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword());
 
-
-
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            String jwt = jwtUtils.generateJwtToken(authentication);
-
-            CustomerDetailImpl customerDetail = (CustomerDetailImpl) authentication.getPrincipal();
-            List<String> roles = customerDetail.getAuthorities().stream()
-                    .map(item -> item.getAuthority())
-                    .collect(Collectors.toList());
+        Authentication authentication = authenticationManager.authenticate(obj);
 
 
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
-            return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("ok","login successfully",new JwtResponse(jwt,
-                    customerDetail.getId(),
-                    customerDetail.getUsername(),
-                    customerDetail.getPassword(),
-                    roles)));
-        }
-        @PostMapping("/signup")
-        public ResponseEntity<ResponseObject>registerUser( @RequestBody Customer customer){
-            Customer found=customerRepo.findByUsername(customer.getUsername());
-            return (found==null)?ResponseEntity.ok()
-                    .body(new ResponseObject("OK","Register successfully",customerServices.registerUser(customer)))
-                  :ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED)
-                    .body(new ResponseObject("Failed","username does exist!!!",new ArrayList<>()));
-        }
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        CustomerDetailImpl customerDetail = (CustomerDetailImpl) authentication.getPrincipal();
+        List<String> roles = customerDetail.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("ok", "login successfully", new JwtResponse(jwt,
+                customerDetail.getId(),
+                customerDetail.getUsername(),
+                customerDetail.getPassword(),
+                roles)));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<ResponseObject> registerUser(@RequestBody Customer customer) {
+        Customer found = customerRepo.findByUsername(customer.getUsername());
+        return (found == null) ? ResponseEntity.ok()
+                .body(new ResponseObject("OK", "Register successfully", customerServices.registerUser(customer)))
+                : ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED)
+                .body(new ResponseObject("Failed", "username does exist!!!", new ArrayList<>()));
+    }
 }
