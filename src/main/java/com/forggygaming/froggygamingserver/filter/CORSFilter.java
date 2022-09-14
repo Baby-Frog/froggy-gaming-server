d4957effc18f25948753ed90b6306a5f85098e95
QuangMinh <93485271+QuangMinh8699@users.noreply.github.com>
2022-09-14 15:25:49 +0700

    create cors configuration

---
+++ src/main/java/com/forggygaming/froggygamingserver/filter/CORSFilter.java
@@ -0,0 +1,45 @@
+package com.forggygaming.froggygamingserver.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component
+public class CORSFilter implements Filter {
+
+    private final Logger log = LoggerFactory.getLogger(CORSFilter.class);
+
+    public CORSFilter() {
+        log.info("CORSFilter init");
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
