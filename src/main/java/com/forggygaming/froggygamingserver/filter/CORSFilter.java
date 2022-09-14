36469ec319913b1eb26cb3b610ff5102a5aafc6a
QuangMinh <93485271+QuangMinh8699@users.noreply.github.com>
2022-09-14 19:25:55 +0700

    Update CORSFilter.java

--- src/main/java/com/forggygaming/froggygamingserver/filter/CORSFilter.java
+++ src/main/java/com/forggygaming/froggygamingserver/filter/CORSFilter.java
@@ -31,7 +31,7 @@ public class CORSFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "PUT, PATCH, POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
 
